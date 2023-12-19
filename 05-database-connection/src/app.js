5e4e0fcd45cd92c5a3de4a0ea0c669bb193ef41f
Harsh <harshdivate45@gmail.com>
2023-12-20 00:01:27 +0530

    routes

--- 05-database-connection/src/app.js
+++ 05-database-connection/src/app.js
@@ -1,19 +1,25 @@
-import express from "express";
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
+import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
+const app = express()
 
-const app = express();
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+)
 
-app.use(cors({
-    origin :process.env.CORS_ORIGIN,
-    credentials:true,
-}))
-
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
-app.use(express.static(("public")))
+app.use(express.json({ limit: '16kb' }))
+app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+app.use(express.static('public'))
 app.use(cookieParser())
 
+//Routes import
+import { router as userRouter } from './routes/users.routes.js'
 
-export {app}
+//routes declartion
+app.use('/api/v1/users', userRouter)
+
+export { app }
--- 05-database-connection/src/utils/asyncHandler.js
+++ 05-database-connection/src/utils/asyncHandler.js
@@ -1,13 +1,12 @@
-const asyncHandler = (func) => async (req,res,next) =>{
+const asyncHandler = (func) => async (req, res, next) => {
     try {
-        await func(req,res,next)
+        return await func(req, res, next)
     } catch (error) {
         res.status(error.code || 500).json({
-            success:false,
-            message :error.message
+            success: false,
+            message: error.message,
         })
     }
-
 }
 
 export default asyncHandler
